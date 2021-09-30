6bf24a96c9c40e892879a02762942b812c0535ef
StineNyhus <stnyl@hotmail.com>
2021-09-30 13:35:40 +0200

    Worked on document frequency function

--- syllabus/classes/class4/class4_prep.py
+++ syllabus/classes/class4/class4_prep.py
@@ -4,16 +4,28 @@ def term_frequency(tokens) -> dict:
     token_count = Counter(tokens)
     return {token:count/len(tokens) for token, count in token_count.items()}
 
+# def doc_frequency(docs) -> dict:
+#     all_tokens = []
+#     for lst in docs:
+#         all_tokens += lst
+#     return dict(Counter(all_tokens))
+
 def doc_frequency(docs) -> dict:
-    all_tokens = []
+    terms = set()
     for lst in docs:
-        all_tokens += lst
-    return dict(Counter(all_tokens))
+        terms = set.union(terms, set(lst))
+    count = {term:0 for term in terms}
+    for lst in docs:
+        for term in terms:
+            if term in lst:
+                count[term] += 1
+    return count
 
 
 if __name__ == "__main__":
-    list_of_lists = [["sa", "dd", "fgj"], ["ffg", "sa"], ["sa", "dd"]]
+    list_of_lists = [["sa", "dd", "sa", "fgj"], ["ffg", "sa"], ["sa", "dd"]]
     list_of_tokens = ["sa", "dd", "fgj", "ffg", "sa", "sa", "dd"]
-    print(term_frequency(list_of_tokens))
+    # print(term_frequency(list_of_tokens))
+    # print(doc_frequency(list_of_lists))
     print(doc_frequency(list_of_lists))
 
